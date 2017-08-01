17da18ca338843027abc2728f59c494f0ca827e9
ctrlf <lfy3715@gmail.com>
2017-08-02 01:24:03 +0800

    关于reducer的组合,action的构造

---
+++ r_d_redux_todo/src/todos/actionTypes.js
@@ -0,0 +1,6 @@
+//增加
+export const ADD_TODO = 'TODO/ADD';
+//反转
+export const TOGGLE_TODO = 'TODO/TOGGLE';
+//删除
+export const REMOVE_TODO = 'TODO/REMOVE';
---
+++ r_d_redux_todo/src/todos/reducer.js
@@ -0,0 +1,26 @@
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from './actionTypes';
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case ADD_TODO: {
+            //对所有可能出现大的action.type
+            return [
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed:false
+                },
+                ...state
+            ]
+        }
+        case TOGGLE_TODO: {
+            return state.map((todoItem) => {
+                if (todoItem.id === action.id) {
+                    return { ...todoItem, completed: !todoItem.completed };
+                } else {
+                    return todoItem;
+                }
+            })
+        }    
+    }
+}
