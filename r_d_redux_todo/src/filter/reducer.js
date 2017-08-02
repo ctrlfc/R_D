e80e632e067f3da62060123d0d5d015595cd5b7f
ctrlf <lfy3715@gmail.com>
2017-08-02 08:43:23 +0800

    组合reducer

---
+++ r_d_redux_todo/src/filter/reducer.js
@@ -0,0 +1,6 @@
+import { SET_FILTER } from "./actionTypes";
+import { FileterTypes } from "../constants.js";
+
+export default (state = FilterTypes.ALL, action) => {
+    
+};
--- r_d_redux_todo/src/todos/reducer.js
+++ r_d_redux_todo/src/todos/reducer.js
@@ -1,26 +1,36 @@
-import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from './actionTypes';
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from "./actionTypes";
 
 export default (state = [], action) => {
-    switch (action.type) {
-        case ADD_TODO: {
-            //对所有可能出现大的action.type
-            return [
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed:false
-                },
-                ...state
-            ]
-        }
-        case TOGGLE_TODO: {
-            return state.map((todoItem) => {
-                if (todoItem.id === action.id) {
-                    return { ...todoItem, completed: !todoItem.completed };
-                } else {
-                    return todoItem;
-                }
-            })
-        }    
+  switch (action.type) {
+    case ADD_TODO: {
+      //对所有可能出现大的action.type
+      //返回一个增加了对象的数组
+      return [
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        },
+        ...state
+      ];
     }
-}
+    case TOGGLE_TODO: {
+      return state.map(todoItem => {
+        if (todoItem.id === action.id) {
+          return { ...todoItem, completed: !todoItem.completed };
+        } else {
+          return todoItem;
+        }
+      });
+    }
+    case REMOVE_TODO: {
+        //过滤掉选择的ID  
+      return state.filter(todoItem => {
+        return todoItem.id !== action.id;
+      });
+    }
+    default: {
+      return state;
+    }
+  }
+};
