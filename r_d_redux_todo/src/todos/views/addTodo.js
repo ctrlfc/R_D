3241c276f9bf42f65c331b8a7e4d70bddc47aad0
ctrlf <lfy3715@gmail.com>
2017-08-03 00:48:23 +0800

    [R_D]addTodo.jsref和dispatch

--- r_d_redux_todo/src/todos/views/addTodo.js
+++ r_d_redux_todo/src/todos/views/addTodo.js
@@ -5,14 +5,22 @@ import {addTodo} from "../action";
 
 class AddTodo extends Component{
     constructor(props,context){
-        super(props,context);
+        super(props, context);
         
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+
+        this.state = {
+            value: ''
+        };
     }
 
     onSubmit(ev) {
+        //取消默认提交行为
         ev.prevenDefault();
 
         const inputValue = this.state.value;
+        //清空格
         if (!inputValue.trim()) {
             return;
         }
@@ -27,14 +35,31 @@ class AddTodo extends Component{
         });
     }
 
+    //运用了ref
     render() {
         return (
             <div className="add-todo">
                 <from onSubmit={this.onSubmit}>
-                <input className=>    
+                    <input className="new-todo" onChange={this.onInputChange
+                    } value={this.state.value} />
+                    <button className="add-btn" type="submit">添加</button>
                 </from>
             </div>
         )
     }
-
 }
+
+AddTodo.propTypes = {
+    onAdd: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAdd: (text) => {
+            dispatch(addTodo(text));
+        }
+    }
+};
+
+export default connect(null, mapDispatchToProps)(AddTodo);
+
