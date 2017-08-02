43a2a02cb7f7befcaef02b47ee645ba24c6f948e
ctrlf <lfy_3715@163.com>
2017-08-02 11:51:00 +0800

    [R_D]view层addtodo

---
+++ r_d_redux_todo/src/todos/views/addTodo.js
@@ -0,0 +1,11 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+import {addTodo} from "../action";
+
+class AddTodo extends Component{
+    constructor(props,context){
+        super(props,context);
+        
+    }
+}
