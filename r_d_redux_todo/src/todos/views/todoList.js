4b4dc5e736beaab2e6f58999c388ff2f8064b14a
ctrlf <lfy_3715@163.com>
2017-08-03 17:55:19 +0800

    [R_D]todolist完

--- r_d_redux_todo/src/todos/views/todoList.js
+++ r_d_redux_todo/src/todos/views/todoList.js
@@ -3,16 +3,57 @@ import { connect } from 'react-redux';
 import TodoItem from './todoItem.js';
 import { toggleTodo, removeTodo } from '../action.js';
 import { FileterTypes } from '../../constants.js';
+import { FilterTypes } from '../../constants';
 
-cosnt TodoList = ({ todos, onToggleTodo, onRemoveTodo }) => {
+const TodoList = ({ todos, onToggleTodo, onRemoveTodo }) => {
     return (
         <ul className="todo-list">
             {
-                todo.map(item)=>(
-            <TodoItem
-                key={item.text}    
-                )
+                todo.map((item) => (
+                    <TodoItem
+                        key={item.id}
+                        text={item.text}
+                        completed={item.completed}
+                        onToggle={() => onToggleTodo(item.id)}
+                        onRemove={() => onRemoveTodo(item.id)}
+                    />
+                ))
             }
         </ul>
-    )
+    );
+};
+
+TodoList.propTypes = {
+    todos:PropTypes.array.isRequired
+};
+
+const selectVisibleTodos = (todos, filter) => {
+    switch (filter) {
+        case FilterTypes.ALL:
+            return todos;
+        case FilterTypes.COMPLETED:
+            return todos.filter(item => item.completed);
+        case FilterTypes.UNCOMPLETED:
+            return todos.filter(item => !item.completed);
+        default:
+            throw new Error('unsupported filter');
+    }
+} 
+
+const mapStateToProps = (state) => {
+    return {
+        todos: selectVisibleTodos(state.todos, state.filter)
+    };
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onToggleTodo: (id) => {
+            dispatch(toggleTodo(id));
+        },
+        onRemoveTodo: (id) => {
+            dispatch(removeTodo(id));
+        }
+    };
+};
+
