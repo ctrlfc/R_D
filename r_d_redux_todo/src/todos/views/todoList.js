fc3f843dca2240ef86cf21ab6e74294c336f9594
ctrlf <lfy_3715@163.com>
2017-08-04 14:34:52 +0800

    [R_D]prop-types提出处理

--- r_d_redux_todo/src/todos/views/todoList.js
+++ r_d_redux_todo/src/todos/views/todoList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import TodoItem from "./todoItem.js";
 import { toggleTodo, removeTodo } from "../action.js";
