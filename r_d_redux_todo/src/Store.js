5a82f5ca8a741b556ff180f5b6a307e013633a2e
ctrlf <lfy3715@gmail.com>
2017-08-05 13:16:56 +0800

    [R_D]插件配置

--- r_d_redux_todo/src/Store.js
+++ r_d_redux_todo/src/Store.js
@@ -1,37 +1,29 @@
 //多个reducer组合
-import {
-    createStore,
-    combineReducers,
-    // applyMiddleware,
-    // compose
-} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
-import { reducer as todoReducer } from './todos/';
-import { reducer as filterReducer } from './filter/';
+import { reducer as todoReducer } from "./todos/";
+import { reducer as filterReducer } from "./filter/";
 
-// import Perf from 'react-addons-perf'
+import Perf from "react-addons-perf";
 
-// const win = window;
-// win.Perf = Perf
+//一般不会去改变全局变量名,所以赋值给一个变量,最后只剩一个全局变量
+const win = window;
+//配和perf插件进行性能测试
+win.Perf = Perf;
 
 const reducer = combineReducers({
-    todos: todoReducer,
-    filter: filterReducer
+  todos: todoReducer,
+  filter: filterReducer
 });
 
-/*  const middlewares = [];
-if (process.env.NODE_ENV !== 'prpduction') {
-    middlewares.push(require('redux-immutable-state-invariant')());  
+const middlewares = [];
+if (process.env.NODE_ENV !== "prpduction") {
+  middlewares.push(require("redux-immutable-state-invariant")());
 }
 
-
 const storeEnhancers = compose(
-    applyMiddleware(...middlewares),
-    (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
-);  */
-
-export default createStore(
-    reducer,
-    // {},
-    // storeEnhancers
+  applyMiddleware(...middlewares),
+  win && win.devToolsExtension ? win.devToolsExtension() : f => f
 );
+
+export default createStore(reducer, {}, storeEnhancers);
